3e38c111f7e47f65be4e43f3238991853974d379
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-06-26 11:56:21 +0530

    C_programe

---
+++ 123.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+    int size =8;
+    for(i=0;i < size;i++);
+    {
+    for(j=0;j < size;j++);
+    }
+    if((i+j) %2 ==0){
+    printf("1 ");
+    }else{
+    printf("0");
+    }
+    return 0;
+}
