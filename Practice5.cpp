8ce5ce1601de23c61ff65b9773ec56e9239afa02
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-09 09:13:07 +0530

    Create Practice5.cpp

---
+++ Practice5.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a;
+int b;
+
+int temp;
+cout<<"enter value a:"<<endl;
+
+cin>>a;
+
+cout<<"enter value b:"<<endl;
+
+cin>>b;
+
+ temp=a;
+
+13 a=b;
+b=temp;
+
+cout<<"after:a="<<a<<" b="<<b<<endl;
+return 0;
+}
