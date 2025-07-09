106a47e3a7cfbdff73bd6d5000ce86d58000d3f6
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-09 09:17:35 +0530

    Create Practice6.cpp

---
+++ Practice6.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+int value;
+
+cout<<"enter value:"<<endl;
+cin>>value;
+
+if(value>0) {
+cout<<"the value is positive"<<endl;
+}
+else if (value<0){
+
+cout<<"the value is nagative"<<endl;
+}
+ else{
+
+14 cout<<"the number is zero"<<endl;
+}
+return 0;
+}
