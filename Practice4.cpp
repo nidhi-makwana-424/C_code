0f598270ea13a35ded29cb326ce5aaff3abe1132
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-09 09:10:39 +0530

    Create Practice4.cpp

---
+++ Practice4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int number;
+
+cout<<"enter a valid number"<<endl;
+ cin>>number;
+
+cout<<"number is "<<number<<endl;
+
+ cout<<" after the post increament by 1 number" <<number++<<endl;
+
+ cout<<" after the pre increament by 1 number"<<+ +number<<endl;
+
+ cout<<"after increasing 1 number" <<number+1<<endl;
+
+ cout<<" after the post decremem by 1 number" <<number--<<endl;
+
+cout<<" after the pre decrement by 1 number" <<--number<<endl;
+
+ cout<<"after increasing 1 number" <<number-1<<endl;
+
+ return 0;
+}
