98793007e73d1ed2523971bc6e2755a5c93feafb
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-09 09:08:39 +0530

    Create Practice3.cpp

---
+++ Practice3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include<limits>
+
+ using namespace std;
+
+ int main() {
+
+ cout<<"the number of min"
+
+<<numeric_limits<int>::min()<<endl;
+
+cout<<"the number of max" <<numeric_limits<int>::max()<<endl;
+
+ return 0;
+}
