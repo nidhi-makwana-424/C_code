221dce99de6beff5bdac4258d570709f72e31fcd
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-09 09:05:54 +0530

    Create Practice1.cpp

---
+++ Practice1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b) {
+return a+b;
+
+}
+
+int main(){ 
+int x,y;
+
+ cout<<"enter first value ";
+cin>>x;
+
+cout<<"enter second value;
+cin>>y;
+
+ cout<<"sum: "<<add(x,y)<<endl;
+
+return 0;
+}
