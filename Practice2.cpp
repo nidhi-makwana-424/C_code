40108f69668c41e29fa072e8fb744d5453001177
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-09 09:07:36 +0530

    Create Practice2.cpp

---
+++ Practice2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+cout<<"size of char"<<sizeof(char)<<" byte"<<endl;
+
+ cout<<"size of boolean"<<sizeof(bool)<<< byte"<<endl;
+
+ cout<<"size of int"<<sizeof(int)<<" byte"<<endl;
+
+ cout<<"size of float"<<sizeof(float)<<" byte"<<endl;
+
+cout<<"size of dubble"<<sizeof(double )<<" byte"<<endl;
+
+return 0;
+}
