9ee7bc301abfce9a310d32d19fd5862a07794a25
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-15 09:50:48 +0530

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,7 @@
-#include<iostream>
-#include"calculator.h"
+#include "calculator.h"
 
-using namespace std;
 int main(){
-char op,a,b;
-
-cout<<"enter an op(+,-,*,/):";
-cin>>op;
-
-cout<<"enter two values;";
-cin>>a>>b;
-
-switch(op){
-case '+':
-  cout<<a<<"+"<<b<<"="<<a+b<<endl;
-  break;
-case '-':
-  cout<<a<<"-"<<b<<"="<<a-b<<endl;
-  break;
-case '*':
-  cout<<a<<"*"<<b<<"="<<a*b<<endl;
-  break;
-case '/':
-  cout<<a<<"/"<<b<<"="<<a/b<<endl;
-  break;
-default:
-  cout<<"invalid value"<<endl;
-  break;
-}
-return 0;
+	show();
+	
+	return 0;
 }
