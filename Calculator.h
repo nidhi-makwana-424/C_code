4c53ce982b8d873696982d67cc3aeb860219fa0f
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-15 09:48:03 +0530

    Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -1,16 +1,42 @@
-#include<iostream>
+#include <iostream>
 
-int add(int a,int b){
-return a+b;
-}
-int subtract(int a,int b){
-return a-b;
-}
+ using namespace std;
 
-int multiply(int a,int b){
-return a*b;
-}
+ inline void show(){	
+ 	 
+ 	 double a,b;
+ 	 char Operator;
+ 	 
+ 	 cout<<"Enter First Value Of a: ";
+ 	 cin>>a;
+ 	 cout<<"Enter Second Value Of b: ";
+ 	 cin>>b;
+ 	 cout<<"Enter Operator(+,-,*,/): ";
+ 	 cin>>Operator;
 
-int division(int a,int b){
-return a/b;
+ 
+ switch(Operator) {
+ 	case '+':
+ 	 cout<<"Addition: "<<a+b;
+     break;
+ 		
+ 	case '-':
+	 cout<<"Substraction: "<<a-b;
+	 break;
+	 
+	case '*':
+	 cout<<"Multiplication: "<<a*b;
+	 break;
+	 
+	case '/':
+	 if(b!=0){
+	 	cout<<"Division: "<<a/b;
+	 }else{
+	 	cout<<"Invalid Division!!";
+	 break;
 }
+	default:
+	 cout<<"Error!!.. invalid Operator";  	
+  
+ }
+ }
