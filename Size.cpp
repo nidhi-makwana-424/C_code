39171b39e32038eebaab9b3f50193d7f30078dba
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-15 09:27:33 +0530

    Size.cpp

--- Size.cpp
+++ Size.cpp
@@ -6,7 +6,7 @@ int main() {
 
 cout<<"size of char"<<sizeof(char)<<" byte"<<endl;
 
- cout<<"size of boolean"<<sizeof(bool)<<< byte"<<endl;
+ cout<<"size of boolean"<<sizeof(bool)<<" byte"<<endl;
 
  cout<<"size of int"<<sizeof(int)<<" byte"<<endl;
 
