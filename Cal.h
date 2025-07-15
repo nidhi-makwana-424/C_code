2ff100a91262dd3f322f797bf8980ec5efac1cc5
nidhi-makwana-424 <sonalk8291@gmail.com>
2025-07-15 08:50:11 +0530

    Cal.h

--- Cal.h
+++ Cal.h
@@ -1,6 +1,16 @@
 #include<iostream>
 
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int division(int a, int b);
+int add(int a,int b){
+return a+b;
+}
+int subtract(int a,int b){
+return a-b;
+}
+
+int multiply(int a,int b){
+return a*b;
+}
+
+int division(int a,int b){
+return a/b;
+}
